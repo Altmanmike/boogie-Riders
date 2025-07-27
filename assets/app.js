9a146463778e449310213b96df32e28a570185d5
Michael ALTMAN <altman_mike@yahoo.fr>
2025-07-27 15:32:05 +0200

    ADD first files to initialize project

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/app.css";
+import Main from "./js/Main";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode>
+);
